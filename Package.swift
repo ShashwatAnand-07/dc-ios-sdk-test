b4631e267ea898625fd3e2f1571d874c1cc2dd07
ShashwatAnand-07 <shashwat.anand@finbox.in>
2026-09-09 11:40:46 +0530

    Fetching zip from github release

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RiskManager",
-            url: "https://risk-manager-android-sdk.s3.ap-south-1.amazonaws.com/RiskManagerIos/2.2.2/RiskManager-20260908173256.xcframework.zip",
+            url: "https://github.com/ShashwatAnand-07/dc-ios-sdk-test/releases/download/v1.0.0/RiskManager-20260908173256.xcframework.zip",
             checksum: "454f03c277b31e3fc896ab979d6390fa3bb3732b9f265634e6f5bbb28fa871d5"
         ),
     ]
